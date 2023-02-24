3b690b052113d256b874ee8881b9f09eca55ca0b
Naeemo <naeemo@qq.com>
2023-02-24 14:06:28 +0800

    feat example usage

--- example/index.ts
+++ example/index.ts
@@ -3,46 +3,67 @@ import { log } from '../src/util/log'
 
 const totalWidth = 800
 const totalHeight = 500
-const output = document.querySelector<HTMLCanvasElement>('#output')
-const ctx = output.getContext('2d')
+const outputVideo = document.querySelector<HTMLVideoElement>('#output')!
 
-output.height = totalHeight
-output.width = totalWidth
-
+let chunks: Blob[] = []
 const mecorder = new Mecorder({
   width: totalWidth,
   height: totalHeight,
   background: 'red',
   fps: 10,
-  onFrame(imageData, pcms) {
-    ctx.putImageData(imageData, 0, 0)
-  },
+  mimeType: 'video/webm',
 })
+
+mecorder.ondataavailable = function (event) {
+  chunks.push(event.data)
+}
+mecorder.onstop = (e) => {
+  console.log('data available after MediaRecorder.stop() called.')
+
+  // const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
+  const blob = new Blob(chunks, { type: 'video/webm' })
+  chunks = []
+  const audioURL = URL.createObjectURL(blob)
+  outputVideo.src = audioURL
+  console.log('recorder stopped')
+}
 
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
-  .then((stream) => {
+  .then(async (stream) => {
     log('debug', 'get camera', stream)
-    const target = document.querySelector<HTMLVideoElement>('#camera')
+    const target = document.querySelector<HTMLVideoElement>('#camera')!
     target.srcObject = stream
-    target.play()
+    await target.play()
 
-    mecorder.addSource({
-      source: target,
-      destLayout: {
-        x: totalWidth / 2,
-        y: 0,
-        width: totalWidth / 2,
-        height: totalHeight,
-      },
+    target.addEventListener('click', function () {
+      mecorder.addSource({
+        source: target,
+        destLayout: {
+          x: totalWidth / 2,
+          y: 0,
+          width: totalWidth / 2,
+          height: totalHeight,
+        },
+      })
     })
+
+    // mecorder.addSource({
+    //   source: target,
+    //   destLayout: {
+    //     x: totalWidth / 2,
+    //     y: 0,
+    //     width: totalWidth / 2,
+    //     height: totalHeight,
+    //   },
+    // })
   })
 
 mecorder.addSource({
-  source: document.querySelector<HTMLVideoElement>('#video'),
+  source: document.querySelector<HTMLVideoElement>('#video')!,
   destLayout: {
     x: 0,
     y: 0,
@@ -51,18 +72,18 @@ mecorder.addSource({
   },
 })
 
-document.querySelector('#startBtn').addEventListener('click', () => {
+document.querySelector('#startBtn')?.addEventListener('click', () => {
   mecorder.start()
 })
 
-document.querySelector('#pauseBtn').addEventListener('click', () => {
+document.querySelector('#pauseBtn')?.addEventListener('click', () => {
   mecorder.pause()
 })
 
-document.querySelector('#resumeBtn').addEventListener('click', () => {
+document.querySelector('#resumeBtn')?.addEventListener('click', () => {
   mecorder.resume()
 })
 
-document.querySelector('#stopBtn').addEventListener('click', () => {
+document.querySelector('#stopBtn')?.addEventListener('click', () => {
   mecorder.stop()
 })
